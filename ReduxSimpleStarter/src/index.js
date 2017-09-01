577a5ef6b8e70a27e945e54f0e8a54ff6887d4a1
Shubham Suri <shubham013@live.com>
2017-09-01 19:19:50 +0530

    added video list

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAdjDDsc_40dnsQq-5RtwiV8Xw-nnMdWxI';
 
-YTSearch({key: API_KEY, term: 'smimey'}, (data) => {console.log(data)});
-const App = function() {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {videos: ''};
+
+    YTSearch({key: API_KEY, term: 'smiley'}, (videos) => {this.setState({videos} ); console.log({videos}) } );
+
+  }
+
+  render(){
   return (<div>hi there......dfcsdfcvdzfcvdsfvvfg
           <SearchBar />
+          <VideoList videos = {this.state.videos} />
           </div>
             );
+  }
 };
 
 
